5bbcce03027dd357027fc5a026c9be5d38b0ae5c
Chad Kieffer <ckieffer@gmail.com>
2010-08-21 10:22:05 -0600

    Restore nextIndex property, not sure why I dropped it. Tighten up regular expressions. Add replaceIndices to handle IE.

--- yui.utilities.js
+++ yui.utilities.js
@@ -5,10 +5,9 @@
  */
 
 var YuiUtilties = {
-  
+  nextIndex : null,
   /**
    * Clone a specified HTML element
-   * @todo Test against IE 8
    */
   cloneElement : function(elID) {
     var oOriginal = document.getElementById(elID);
@@ -19,7 +18,11 @@ var YuiUtilties = {
   },
   getCountByClassName : function(className) {
     var count = YAHOO.util.Dom.getElementsByClassName(className).length;
-    return count;
+    if (count > 0) {
+      return count;
+    } else {
+      return 0;
+    }
   },
   /**
    * Increment CSS ID and field names for an element and its child elements
@@ -27,28 +30,50 @@ var YuiUtilties = {
   incrementIndices : function(el, index) {
     if (index == 0) {
       var strClassName = el.className;
-      var nextIndex = this.getCountByClassName(strClassName) + 1;
+      this.nextIndex = this.getCountByClassName(strClassName) + 1;
       // Remove index number from the element's id
-       el.id = this.removeIndex(el.id) + nextIndex;
+      el.id = this.removeIndex(el.id) + this.nextIndex;
     }
-    // Loop through the element's children
-    var children = YAHOO.util.Dom.getChildren(el);
-    for (var i=0; i<children.length; i++) {
-      children[i].id = this.removeIndex(children[i].id) + nextIndex;
-      // Handle form element names
-      if (children[i].hasAttribute('name')) {
-        children[i].setAttribute('name', this.removeIndex(children[i].id) + nextIndex);
-        children[i].value = '';
-      }
-      // Continue on with children of children
-      if (children[i].hasChildNodes()) {
-        this.incrementIndices(children[i], nextIndex);
+    // Handle IE
+    if (YAHOO.env.ua.ie > 0) {
+      var thisInnerHTML = el.innerHTML;
+      thisInnerHTML = this.replaceIndices(thisInnerHTML);
+      alert(thisInnerHTML);
+      el.innerHTML = thisInnerHTML;
+    // Other browsers
+    } else {
+      // Loop through the element's children
+      var children = YAHOO.util.Dom.getChildren(el);
+      for (var i=0; i<children.length; i++) {
+        // Set various attributes, removed cloned form input values
+        if (children[i].hasAttribute('id')) {
+          children[i].id = this.removeIndex(children[i].id) + this.nextIndex;
+        }
+        if (children[i].hasAttribute('name')) {
+          children[i].name = this.removeIndex(children[i].name) + this.nextIndex;
+          children[i].value = '';
+        }
+        // Continue on with children of children
+        if (children[i].hasChildNodes()) {
+          this.incrementIndices(children[i], this.nextIndex);
+        }
       }
     }
     return el;
   },
+  /**
+   * Remove an index off the end of an id or name attribute, ex. my_field1 > my_field
+   */
   removeIndex : function(str) {
-    return str.replace(/[0-9]/, '');
+    return str.replace(/^([\w_-]*)[0-9]+$/i, "$1");
+  },
+  /**
+   * In IE do a wholesale removal of form field values, replace 
+   * indices at the end of id and name attributes
+   */
+  replaceIndices : function(str) {
+    str = str.replace(/(value=.[^\s]*)/gi, "");
+    return str.replace(/([\w_-]*)[0-9]+/g, "$1" + this.nextIndex);
   },
   /**
    * Show selected or entered values
