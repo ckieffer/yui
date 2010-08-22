3e65cfd04c5a2cbe65526c1761652562fcb6f3b4
Chad Kieffer <ckieffer@gmail.com>
2010-08-22 13:16:26 -0600

    White-space updates.

--- yui.calendar.js
+++ yui.calendar.js
@@ -2,47 +2,49 @@
  * Initialize YUI Calendar widgets
  * 
  * @author Chad Kieffer, ckieffer at gmail dot com
- * Based on and inspired by code from Brad Harris http://bit.ly/aOmbYA
- *   and Christian Heilmann http://icant.co.uk/sandbox/eventdelegation
  * 
- * @link http://developer.yahoo.com/yui/calendar/
+ * Based on and inspired by posts from Brad Harris http://bit.ly/aOmbYA
+ * and Christian Heilmann bit.ly/aFyJ20
+ * 
+ * @see http://developer.yahoo.com/yui/calendar/
+ * @see http://2tbsp.com/yui_calendar_widget
  *
- * @todo Write a usage blog post, documentation
- *
+ * @todo Properly set the current date value on the calendar for the current field
  * Progressive enhancement
- * @todo Add "choose" button/link through the widget
- * @todo Apply the calendar container div to the input instead of requiring it be hard-coded
- *
+ * @todo Add "choose" button/link dynamically through the widget
+ * @todo Apply the calendar container div to the input instead of requiring it be hardcoded
  * Interactivity/Accessibility
- * @todo Clicking toggle buttons a second time, or clicking anywhere but the calendar, should close the calendar
- * @todo Add a second mode which triggers date picker when a date field gains focus
+ * @todo Close the calendar when toggle button is clicked a second time, or clicking anywhere
+ * @todo Trigger the date picker when a date field gains focus
  * @todo Evaluate and add accessibility features
  */
 
 YuiCalendar = {
-  
-  config : {
+
+  // Default YUI calendar widget options
+  // Used if yuiConfig.calendar isn't set
+  config: {
     title: 'Choose a date:',
     close: true,
     navigator: true
   },
-  dateFormat : 'MM/DD/YYYY',
-  calendarWidget : null,
-  container : 'calendar_container',
-  calendarID : 'yui_calendar',
-  activeInput : null,
-  btn : null,
-  btnContainer : null,
-  btnContainerClass : 'btn_container',
-  btnClassRegEx : new RegExp('choose_date'),
+  dateFormat: 'MM/DD/YYYY',
+  calendarWidget: null,
+  container: 'calendar_container',
+  calendarID: 'yui_calendar',
+  activeInput: null,
+  btn: null,
+  btnContainer: null,
+  btnContainerClass: 'btn_container',
+  btnClassRegEx: new RegExp('choose_date'),
   
-  initialize : function() {
+  initialize: function() {
     var self = YuiCalendar;
     self.setConfig();
     self.renderContainer();
     YAHOO.util.Event.delegate(this, 'click', self.click, 'button');
   },
-  click : function(e) {
+  click: function(e) {
     var self = YuiCalendar;
     self.btn = YAHOO.util.Event.getTarget(e);
     if (self.btn.id.match(self.btnClassRegEx)) {
@@ -55,7 +57,7 @@ YuiCalendar = {
       self.positionCalendar();
     }
   },
-  setConfig : function() {
+  setConfig: function() {
     if (typeof(yuiConfig) != 'undefined' && typeof(yuiConfig.calendar) != 'undefined') {
       this.config = yuiConfig.calendar;
       if (typeof(yuiConfig.calendar.dateFormat) == 'undefined') {
@@ -65,13 +67,13 @@ YuiCalendar = {
       this.config = YuiCalendar.config;
     }
   },
-  renderContainer : function() {
+  renderContainer: function() {
     var container = document.createElement('div');
     container.id = this.container;
     container.style.display = 'none';
     document.body.appendChild(container);
   },
-  renderCalendar : function() {
+  renderCalendar: function() {
     this.calendarWidget = new YAHOO.widget.Calendar(
         this.calendarID,
         this.container,
@@ -80,12 +82,12 @@ YuiCalendar = {
     this.calendarWidget.selectEvent.subscribe(this.populateDateField, this, true);
     this.calendarWidget.render();
   },
-  positionCalendar : function() {
+  positionCalendar: function() {
     var position = YAHOO.util.Dom.getXY(YAHOO.util.Dom.get(this.activeInput));
     position[1] = position[1] + 25;
     YAHOO.util.Dom.setXY(this.container, position);
   },
-  populateDateField : function() {
+  populateDateField: function() {
     var date = this.calendarWidget.getSelectedDates()[0];
     var formattedDate = this.config.dateFormat.replace(/MM/i, date.getMonth());
     formattedDate = formattedDate.replace(/DD/i, date.getDate());
@@ -93,7 +95,7 @@ YuiCalendar = {
     YAHOO.util.Dom.get(this.activeInput).value = formattedDate;
     this.calendarWidget.hide();
   },
-  hide : function() {
+  hide: function() {
     if (this.calendarWidget !== null) {
       this.calendarWidget.hide();
     }
